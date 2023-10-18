89b6aeca016c636f793d72f916665a703bc0903f
memmalino <memmalino@gmail.com>
2023-10-18 01:34:57 +0000

    base_model, file_storage, __init__

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,56 @@
 #!/usr/bin/python3
 
-import uuid
+""" This module defines the Basemodel class """
+
+import models
+from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        """Initialize a new instance. """
-        if kwargs:
-            for key, value in kwargs.item():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-    def save(self):
-        """Update the updated_at attribute with the current date time. """
-        self.update_at - datetime.now()
-
-    def to_dict(self):
-        """Return a dictionary representation of the instance."""
-        obj_dict = self.__dict__.copy()
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        obj_dict["__clas__"] = self.__class__.__name__
-        return obj_dict
     
+    """ The BaseModel for the AirBnb Project """
+    
+    def __init__(self, *args, **kwargs):
+
+        """ 
+            This function initializes a new BaseModel
+            
+            Args:
+                *args (any): unused.
+                **kwargs (dict): Key/value pairs of attributes
+        """
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, t_format)
+                else:
+                    self.__dict__[key] = value
+        else:
+           models.storage.new(self)
     def __str__(self):
-        """Return a string representaiton of the instance."""
-         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        
+        """ function that return a string version of the class """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+    def save(self):
+        
+        """ Function that update the public instance attribute updated_at to current datetime """
+        self.updated_at = datetime.now()
+        models.storage.save()
+    def to_dict(self):
+        
+        """
+        Return the dictionary of the BaseModel instance.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+
+        """
+        rt_dict = self.__dict__.copy()
+        rt_dict["created_at"] = self.created_at.isoformat()
+        rt_dict["updated_at"] = self.updated_at.isoformat()
+        rt_dict["__class__"] = self.__class__.__name__
+        return (rt_dict)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+""" This is the File Storage module."""
+
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+
+    """The file storage class """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        
+        """ It returns the dictionary __objects """
+
+        return (self.__objects)
+
+    def new(self, obj):
+
+        """
+        sets in __objects the obj with key
+        <obj class name>.id
+        
+        """
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+    def save(self):
+
+        """ serializes __objects to the JSON file (path: __file_path) """
+        
+        with open(self.__file_path, 'w') as json_file:
+            tmp_dict = {}
+            for key, value in self.__objects.items():
+                tmp_dict[key] = value.to_dict()
+            json.dump(tmp_dict, json_file)
+        
