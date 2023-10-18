a1589063120cdac010f4313a619e6170b2452e4f
memmalino <memmalino@gmail.com>
2023-10-18 02:18:15 +0000

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,13 +5,14 @@
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
 
     """The file storage class """
 
     __file_path = "file.json"
     __objects = {}
-
+    
     def all(self):
         
         """ It returns the dictionary __objects """
@@ -23,11 +24,11 @@ class FileStorage:
         """
         sets in __objects the obj with key
         <obj class name>.id
-        
+
         """
         self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
-    def save(self):
 
+    def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
         
         with open(self.__file_path, 'w') as json_file:
@@ -35,4 +36,3 @@ class FileStorage:
             for key, value in self.__objects.items():
                 tmp_dict[key] = value.to_dict()
             json.dump(tmp_dict, json_file)
-        
