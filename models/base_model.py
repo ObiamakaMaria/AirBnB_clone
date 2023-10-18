0a64b16fcb5b8bcbf2afd3d2bd9599f9352c9a1c
memmalino <memmalino@gmail.com>
2023-10-18 02:02:28 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     
     def __init__(self, *args, **kwargs):
 
-    """
+        """
         This function initializes a new BaseModel
         
         Args:
@@ -32,18 +32,21 @@ class BaseModel:
                     self.__dict__[key] = value
         else:
             models.storage.new(self)
+
     def __str__(self):
         
         """ function that return a string version of the class """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
     def save(self):
         
-        """ Function that update the public instance attribute updated_at to current datetime """
+        """ update the public instance attribute updated_at to current datetime """
         self.updated_at = datetime.now()
         models.storage.save()
+
     def to_dict(self):
-        
+    
         """
         Return the dictionary of the BaseModel instance.
         Includes the key/value pair __class__ representing
