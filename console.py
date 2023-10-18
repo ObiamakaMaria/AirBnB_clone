e84b20bd4f8d0b754b468569531d5f0f4ee41265
memmalino <memmalino@gmail.com>
2023-10-18 16:13:29 +0000

    updated console.py added now

---
+++ console.py
@@ -0,0 +1,18 @@
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """The entry point of the command interpreter"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit on EOF"""
+        print("")
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
