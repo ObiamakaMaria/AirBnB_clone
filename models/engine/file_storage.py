7ed76aef5bc613b7f0ebb9d5c4e9c9672eea01ab
memmalino <memmalino@gmail.com>
2023-10-21 12:01:45 +0000

    updated file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,21 +26,26 @@ class FileStorage:
         <obj class name>.id
 
         """
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
-        with open(self.__file_path, 'w') as json_file:
-            tmp_dict = {}
-            for key, value in self.__objects.items():
-                tmp_dict[key] = value.to_dict()
-            json.dump(tmp_dict, json_file)
+        serialized_objects = {}
+        for key, value in self.__objects.items():
+            serialized_objects[key] = value.to_dict()
+        with open(self.__file_path, "w") as files:
+            json.dump(serialized_objects, files)
 
     def reload(self):
         """ deserializes the JSON file to __objects """
         try:
-            with open(self.__file_path, 'r') as rt_json:
-                for obj in json.load(rt_json).values():
-                    self.new(eval(obj["__class__"])(**obj))
+            with open(self.__file_path, "r") as files:
+                data = json.load(files)
+                for key, value in data.items():
+                    class_name, obj_id = key.split(".")
+                    cls = eval(class_name)
+                    obj = cls(**value)
+                    self.__objects[key] = obj
         except FileNotFoundError:
-            return
+            pass
