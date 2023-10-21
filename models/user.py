d5060cc8a06057d378d0cbc92254d9c129fd16e8
memmalino <memmalino@gmail.com>
2023-10-21 20:20:45 +0000

    updated executable

--- models/user.py
+++ models/user.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-
-""" This is the user Module.
-contains the user class that inherits from BaseModel
+"""This is the User Model module.
+Contains the User class that inherits from BaseModel.
 """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ A class that define a User.
+    """This class defines a User.
     Attributes:
         email (str): the user's email.
         password (str): the user's password.
         first_name (str): the user's first_name.
         last_name (str): the user's last_name.
     """
+
     email = ""
-    pasword = ""
+    password = ""
     first_name = ""
     last_name = ""
